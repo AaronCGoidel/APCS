42d90e1d865fcc5b3167ace03c54be9336ef9c50
AaronCGoidel <agoidel2019@mka.org>
2017-09-02 20:46:34 -0400

    Summer Work
    Hello World and set up package structure

---
+++ src/com/AaronCGoidel/APCS/summer_work/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.AaronCGoidel.APCS.summer_work;
+
+/**
+ * Created by agoidel2019 on 9/2/17.
+ */
+public class HelloWorld
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World");
+    }
+}
