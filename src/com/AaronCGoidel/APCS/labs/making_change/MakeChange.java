5f4686814f877634da971604c61b2593ef6589fe
AaronCGoidel <agoidel2019@mka.org>
2017-09-27 12:12:21 -0400

    Making Change homework

---
+++ src/com/AaronCGoidel/APCS/labs/making_change/MakeChange.java
@@ -0,0 +1,44 @@
+package com.AaronCGoidel.APCS.labs.making_change;
+
+import java.util.Scanner;
+
+public class MakeChange {
+    public static double amountDue(double costOfGoods, double amountPaid){
+        return roundOff(amountPaid-costOfGoods);
+    }
+
+    public static double roundOff(double toRound)
+    {
+        return Math.round(toRound * 100.0) / 100.0;
+    }
+
+    public static void main(String[] args){
+        Scanner getIn = new Scanner(System.in);
+
+        double DENOMINATIONS[] =  {1.00, .25, .10, .05, .01};
+        int currentDenomination = 0;
+        int numberOfAmount;
+
+        System.out.print("Enter Price\n$");
+        double cost = getIn.nextDouble();
+        System.out.print("Enter Amount Paid\n$");
+        double paid = getIn.nextDouble();
+
+        double due = amountDue(cost, paid);
+
+        if(due<0){
+            System.out.println("THIEF!!!");
+        } else if(due==0){
+            System.out.println("Perfect Change.");
+        } else{
+            System.out.println(due);
+            while(due>0){
+                numberOfAmount = (int) Math.floor(due/DENOMINATIONS[currentDenomination]);
+                due-=numberOfAmount*DENOMINATIONS[currentDenomination];
+                System.out.println(String.format("$%.2f: %d", DENOMINATIONS[currentDenomination], numberOfAmount));
+                currentDenomination += 1;
+            }
+        }
+    }
+}
+
---
+++ src/com/AaronCGoidel/APCS/labs/making_change/SelfCheckout.java
@@ -0,0 +1,30 @@
+package com.AaronCGoidel.APCS.labs.making_change;
+
+import java.util.Scanner;
+
+public class SelfCheckout
+{
+    public static double roundOff(double toRound)
+    {
+        return Math.round(toRound * 100.0) / 100.0;
+    }
+    public static void main(String[] args)
+    {
+        Scanner getInput = new Scanner(System.in);
+
+        double total = 0;
+        double unitPrice, quantity, subtotal;
+        for(int i=0; i<10;i++)
+        {
+            System.out.println(String.format("Item #%d", i+1));
+            System.out.print("Enter Price\n$");
+            unitPrice = getInput.nextDouble();
+            System.out.print("Enter Number of Items\n>");
+            quantity = getInput.nextDouble();
+            subtotal = roundOff(unitPrice * quantity);
+            total += subtotal;
+            System.out.println(String.format("Subtotal: $%.2f", subtotal));
+        }
+        System.out.println(String.format("Total: $%.2f", total));
+    }
+}
