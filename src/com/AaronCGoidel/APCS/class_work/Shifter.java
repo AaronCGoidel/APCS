e2bf87a0b3825b5178607186c983edf651a607e9
AaronCGoidel <agoidel2019@mka.org>
2017-10-26 09:16:26 -0400

    slight reformat

--- src/com/AaronCGoidel/APCS/class_work/Shifter.java
+++ src/com/AaronCGoidel/APCS/class_work/Shifter.java
@@ -2,9 +2,23 @@ package com.AaronCGoidel.APCS.class_work;
 
 public class Shifter
 {
+    public static char shifter(char original, int offset)
+    {
+        offset = offset % 26 + 26;
+
+        if(Character.isLetter(original)){
+            if(Character.isUpperCase(original)){
+                return ((char) ('A' + (original - 'A' + offset) % 26));
+            }else{
+                return ((char) ('a' + (original - 'a' + offset) % 26));
+            }
+        }else{
+            return (original);
+        }
+    }
+
     public static void main(String[] args)
     {
-
         String str = "The quick brown fox Jumped over the lazy Dog";
 
         for(int i = 0; i < str.length(); i++)
@@ -12,21 +26,4 @@ public class Shifter
             System.out.print(shifter(str.charAt(i), 12));
         }
     }
-
-
-    public static char shifter(char original, int offset)
-    {
-        offset = offset % 26 + 26;
-
-            if(Character.isLetter(original)){
-                if(Character.isUpperCase(original)){
-                    return ((char) ('A' + (original - 'A' + offset) % 26));
-                }else{
-                    return ((char) ('a' + (original - 'a' + offset) % 26));
-                }
-            }else{
-                return (original);
-            }
-        }
-
 }
